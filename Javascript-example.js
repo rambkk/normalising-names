9b62cfe4bacaf81334954f2fe4f1cc7cca5cb81f
rambkk <37767945+rambkk@users.noreply.github.com>
2021-10-20 20:32:21 +0700

    Create Javascript-example.js

---
+++ Javascript-example.js
@@ -0,0 +1,17 @@
+/* Javascript sample function to normalise name
+ * using rules from https://github.com/rambkk/normalising-names
+ *
+ * normaliseName (version 0.11 - initial release)
+ * 
+ * (c) Ram Narula 
+ * You can use this information, do give credit: 
+ * github rambkk - Ram Narula - pluslab.net
+ */
+
+var myString="hello \u2013\u001F\u200B\u200C\u200D";
+var finalString;
+
+finalString=myString.replace(/[\u0000-\u001F\u007f\u00AD\u200B-\u200D\u3000\uFEFF]/g,'');
+finalString=myString.replace(/\u00AD/,' ');
+finalString=myString.replace(/\u2013/,'-');
+
